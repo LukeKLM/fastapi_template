2136d9a4aebfd7e4873a0020dac2b8d3b2780864
Lukáš Klíma <Lukas.klm94@gmail.com>
2024-11-03 10:28:04 +0100

    init: models import into alembic

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,1 @@
-from app.models.customers import InvoiceCustomer  # noqa
-from app.models.invoice_items import InvoiceItem  # noqa
 from app.models.invoices import Invoice  # noqa
-from app.models.suppliers import InvoiceSupplier  # noqa
